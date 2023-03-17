97151e157947c3d9935f474e7700e8d63bdc0825
JefersonArdila <yefersonardila100@gmail.com>
2023-03-17 12:12:07 -0500

    Mostrar del api solo el id, nombre y genero

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,15 +1,32 @@
 
 const fetchApi = async (url) => {
     try{
+        
         const res = await fetch(url);
         const data = await res.json()
-        console.log(data);
+        .then(data => {
+            
+            // Recorra cada objeto de carácter y extraiga el id, el nombre y el genero
+            data.results.forEach(character => { 
+            console.log(`ID: ${character.id}, Name: ${character.name}, Gender: ${character.gender}`);
+            })
+            
+        })
+        
+        
+        
+        
+        
+        
     }catch(error){
 console.log(error);
+
     }
 }
+
 const gretings = () => {
     console.log('hello there')
+    
 }
 
 
