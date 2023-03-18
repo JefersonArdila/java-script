791cac6e4646d975b4a21ab6edca88c99db58ff7
JefersonArdila <yefersonardila100@gmail.com>
2023-03-18 11:03:25 -0500

    clase 2

---
+++ index.js
@@ -0,0 +1,28 @@
+const { write } = require("fs");
+const http = require("http");
+
+const HOST = "localhost";
+const PORT = 8000;
+
+const writeHtmlResponse = (res, htmlCode) => {
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(200);
+    res.end(htmlCode);
+}
+
+const server = http.createServer((req,res)=>{
+
+    const url = req.url;
+    console.log("URL es", url );
+
+    if(url === "/other"){
+        writeHtmlResponse(res, "<p> Esta es otra ruta </p>");
+    }else{
+        writeHtmlResponse(res, "<p> codigo HTML </p>");
+    }
+});
+
+server.listen(PORT,HOST,() =>{
+    console.log(`Servidor corriendo en + 
+    http://${HOST}:${PORT}`);
+});
